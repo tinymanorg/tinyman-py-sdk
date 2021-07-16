de38ca3e320515eb7bd10ddece54352580c84f20
Fergal Walsh <fergalwalsh@gmail.com>
2021-07-16 14:13:05 +0100

    Make get_swap_transactions work for ASA-ASA or ASA-Algo

--- tinyman/v1/swap.py
+++ tinyman/v1/swap.py
@@ -5,18 +5,19 @@ from algosdk.future.transaction import ApplicationNoOpTxn, PaymentTxn, AssetTran
 from algosdk.v2client.algod import AlgodClient
 
 from .contracts import validator_app_def, get_pool_logicsig
-from tinyman.utils import int_to_bytes
 
 
-def get_swap_with_algo_transactions(client: AlgodClient, sender, validator_app_id, asset1_id, liquidity_asset_id, asset_in_id, asset_in_amount, asset_out_amount, swap_type):
+def get_swap_transactions(client: AlgodClient, sender, validator_app_id, asset1_id, asset2_id, liquidity_asset_id, asset_in_id, asset_in_amount, asset_out_amount, swap_type):
     suggested_params = client.suggested_params()
-    pool_logicsig = get_pool_logicsig(validator_app_id, asset1_id, 0)
+    pool_logicsig = get_pool_logicsig(validator_app_id, asset1_id, asset2_id)
     pool_address = pool_logicsig.address()
 
     swap_types = {
         'fixed_input': 'fi',
         'fixed_output': 'fo',
     }
+
+    asset_out_id = asset2_id if asset_in_id == asset1_id else asset1_id
 
     txns = [
         PaymentTxn(
@@ -31,15 +32,15 @@ def get_swap_with_algo_transactions(client: AlgodClient, sender, validator_app_i
             index=validator_app_id,
             app_args=['swap', swap_types[swap_type]],
             accounts=[sender],
-            foreign_assets=[asset1_id, liquidity_asset_id],
+            foreign_assets=[asset1_id, liquidity_asset_id] if asset2_id == 0 else [asset1_id, asset2_id, liquidity_asset_id],
         ),
         AssetTransferTxn(
             sender=sender,
             sp=suggested_params,
             receiver=pool_address,
             amt=int(asset_in_amount),
-            index=asset1_id,
-        ) if asset_in_id == asset1_id else PaymentTxn(
+            index=asset_in_id,
+        ) if asset_in_id != 0  else PaymentTxn(
             sender=sender,
             sp=suggested_params,
             receiver=pool_address,
@@ -50,8 +51,8 @@ def get_swap_with_algo_transactions(client: AlgodClient, sender, validator_app_i
             sp=suggested_params,
             receiver=sender,
             amt=int(asset_out_amount),
-            index=asset1_id,
-        ) if asset_in_id == 0 else PaymentTxn(
+            index=asset_out_id,
+        ) if asset_out_id != 0 else PaymentTxn(
             sender=pool_address,
             sp=suggested_params,
             receiver=sender,

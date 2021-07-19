eca9914d5066923fac148decfd31f5956209d755
Fergal Walsh <fergalwalsh@gmail.com>
2021-07-19 11:52:01 +0100

    Fix package_data in setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
     install_requires=["py-algorand-sdk >= 1.6.0"],
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
-    package_data={'': ['tinyman/v1/contracts.json']},
+    package_data={'tinyman.v1': ['contracts.json']},
     include_package_data=True,
 )

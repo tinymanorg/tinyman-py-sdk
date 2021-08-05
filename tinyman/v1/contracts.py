d03bb9256f5ca1d7289d5858a1be5a72c0c1dd6a
Fergal Walsh <fergalwalsh@gmail.com>
2021-08-05 08:45:59 +0100

    Ensure correct ordering of assets in get_pool_logicsig

--- tinyman/v1/contracts.py
+++ tinyman/v1/contracts.py
@@ -12,9 +12,12 @@ validator_app_def = _contracts['contracts']['validator_app']
 
 
 def get_pool_logicsig(validator_app_id, asset1_id, asset2_id):
+    assets = [asset1_id, asset2_id]
+    asset_id_1 = max(assets)
+    asset_id_2 = min(assets)
     program_bytes = get_program(pool_logicsig_def, variables=dict(
         validator_app_id=validator_app_id,
-        asset_id_1=asset1_id,
-        asset_id_2=asset2_id,
+        asset_id_1=asset_id_1,
+        asset_id_2=asset_id_2,
     ))
     return LogicSig(program=program_bytes)

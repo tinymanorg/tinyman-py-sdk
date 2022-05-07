73e7d3a5073a699a014612ea18b55f39e58d73c5
jmrMOONBOI <95954677+jmrMOONBOI@users.noreply.github.com>
2022-05-07 17:37:26 -0400

    Allowed pool retrieval for TMv1.0
    Incremented minor version in setup.py. Removed asc.json and replaced it with two files, one for TM1.0 and one for TM1.1. Added constants to identify the strings for these files in constants.py. Removed unused import from optout.py. Updated contracts.py to read in the asc_file appropriate to client version being used.

--- tinyman/v1/contracts.py
+++ tinyman/v1/contracts.py
@@ -3,15 +3,17 @@ import importlib.resources
 from algosdk.future.transaction import LogicSig
 import tinyman.v1
 from tinyman.utils import get_program
-
-_contracts = json.loads(importlib.resources.read_text(tinyman.v1, 'asc.json'))
-
-pool_logicsig_def = _contracts['contracts']['pool_logicsig']['logic']
-
-validator_app_def = _contracts['contracts']['validator_app']
-
+from .constants import ASC_FILE_V1_0, ASC_FILE_V1_1
 
 def get_pool_logicsig(validator_app_id, asset1_id, asset2_id):
+    if int(validator_app_id) == 350338509:
+        use_asc = ASC_FILE_V1_0
+    elif int(validator_app_id) == 552635992:
+        use_asc = ASC_FILE_V1_1
+    contracts = json.loads(importlib.resources.read_text(tinyman.v1, use_asc))
+    pool_logicsig_def = contracts['contracts']['pool_logicsig']['logic']
+    validator_app_def = contracts['contracts']['validator_app']
+
     assets = [asset1_id, asset2_id]
     asset_id_1 = max(assets)
     asset_id_2 = min(assets)

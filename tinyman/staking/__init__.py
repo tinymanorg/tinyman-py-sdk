7f5e30392f753cc10c7acdcbc0c54ddf46ca5a21
Goksel Coban <goksel326@gmail.com>
2022-12-15 19:47:11 +0300

    remove redundant functions

--- tinyman/staking/__init__.py
+++ tinyman/staking/__init__.py
@@ -26,35 +26,6 @@ from tinyman.utils import (
 from tinyman.staking.constants import DATE_FORMAT
 
 
-# def prepare_create_transaction(args, sender, suggested_params):
-#     from .contracts import staking_app_def
-#
-#     txn = ApplicationCreateTxn(
-#         sender=sender,
-#         sp=suggested_params,
-#         on_complete=OnComplete.NoOpOC.real,
-#         approval_program=b64decode(staking_app_def["approval_program"]["bytecode"]),
-#         clear_program=b64decode(staking_app_def["clear_program"]["bytecode"]),
-#         global_schema=StateSchema(**staking_app_def["global_state_schema"]),
-#         local_schema=StateSchema(**staking_app_def["local_state_schema"]),
-#         app_args=args,
-#     )
-#     return TransactionGroup([txn])
-#
-#
-# def prepare_update_transaction(app_id: int, sender, suggested_params):
-#     from .contracts import staking_app_def
-#
-#     txn = ApplicationUpdateTxn(
-#         index=app_id,
-#         sender=sender,
-#         sp=suggested_params,
-#         approval_program=b64decode(staking_app_def["approval_program"]["bytecode"]),
-#         clear_program=b64decode(staking_app_def["clear_program"]["bytecode"]),
-#     )
-#     return TransactionGroup([txn])
-
-
 def prepare_commit_transaction(
     app_id: int,
     program_id: int,
